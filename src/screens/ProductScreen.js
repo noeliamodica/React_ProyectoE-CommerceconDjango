e43660b5bcc8e1572ce36821476cfcfd115db7c0
noeliamodica <113479035+noeliamodica@users.noreply.github.com>
2023-04-01 18:56:09 -0300

    trabajo en ProductScreen

--- src/screens/ProductScreen.js
+++ src/screens/ProductScreen.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Link, useParams } from 'react-router-dom';
-import {Row, Col, Image, ListGroup, Button, Card }  from 'react-bootstrap';
+import {Row, Col, Image, ListGroup, Button, Card, ListGroupItem }  from 'react-bootstrap';
 import products from '../products';
 
 
@@ -11,7 +11,57 @@ export default function ProductScreen({match} ) {
 
   return (
     <div>
-        {product.name}
+        <Link to='/' className='btn btn-light my-3' > Go back </Link>
+        <Row>
+            <Col md={6} >
+                <Image src={product.image}  alt={product.name} fluid />
+            </Col>
+
+            <Col md={3} >
+                <ListGroup variant='flush'>
+                    <ListGroup.Item>
+                        <h3> {product.name} </h3>
+                    </ListGroup.Item>
+
+                    <ListGroup.Item>
+                       <p> {product.dressing}  </p>  
+                    </ListGroup.Item>
+
+                    <ListGroup.Item>
+                       <p> {product.description}  </p>
+                    </ListGroup.Item>
+
+                    <ListGroup.Item>
+                      Price: ${product.price}
+                    </ListGroup.Item>
+
+                </ListGroup>
+            </Col>
+
+            <Col md={3} >
+                <Card>
+                    <ListGroupItem variant='flush'>
+                        <ListGroup.Item>
+                            <Row>
+                                <Col> Price:  </Col>
+                                <Col> 
+                                    <strong> ${product.price} </strong>
+                                </Col>
+                            </Row>
+                        </ListGroup.Item>
+
+                        <ListGroup.Item>
+                            <Row>
+                                <Col> Status:  </Col>
+                                <Col> 
+                                    In Stock
+                                </Col>
+                            </Row>
+                        </ListGroup.Item>
+                    </ListGroupItem>
+                </Card>
+            </Col>
+        </Row>
     </div>
   )
 }
