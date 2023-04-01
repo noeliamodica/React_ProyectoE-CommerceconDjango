9f767e855b8e5116cde71e2b2ad0ef89178c6e7f
noeliamodica <113479035+noeliamodica@users.noreply.github.com>
2023-04-01 14:22:29 -0300

    trabajo en Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,29 @@
 import React from 'react'
+import {Container, Nav, Navbar, Row }  from 'react-bootstrap';
+
+
 
 function Header() {
   return (
     <div>
-        <header> Header </header>
+        <header> 
+     
+    <Navbar bg="dark" variant='dark' expand="lg" collapseOnSelect>
+      <Container>
+        <Navbar.Brand href="/">ProShop</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="/cart">Cart</Nav.Link>
+            <Nav.Link href="/login">Login</Nav.Link>
+          
+          </Nav>
+            </Navbar.Collapse>
+             </Container>
+         </Navbar>
+
+
+        </header>
 
     </div>
   )
