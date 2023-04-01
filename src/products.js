aedd6cde0c0d6b2d6146ead9254ddc20240eb960
noeliamodica <113479035+noeliamodica@users.noreply.github.com>
2023-04-01 15:53:15 -0300

    genero db inicial en products

---
+++ src/products.js
@@ -0,0 +1,100 @@
+
+     const products = [
+        {
+            "id": 1,
+            "name": "Clásica",
+            "base": "Mix de verdes",
+            "dressing": "Ranch",
+            "description": "Mix de verdes, pollo, huevo, zanahoria, queso sardo rayado, tomates cherrys y aderezo ranch.",
+            "image": "/img/Clásica.jepg",
+            "price": 850,
+            "status": "MasVendido"
+        },
+        {
+            "id": 2,
+            "name": "Cala",
+            "base": "Rúcula y fideos(penne rigatti)",
+            "dressing": "Mostaza y miel",
+            "description": "Rúcula, penne rigatti, pollo, tomates secos, nueces, queso azul y aderezo de mostaza y miel.",
+            "image": "/img/Cala.jepg",
+            "price": 850,
+            "status": "MasVendido"
+        },
+        {
+            "id": 3,
+            "name": "Caesar",
+            "base": "Mix de verdes",
+            "dressing": "Caesar",
+            "description": "Mix de verdes, pollo, croûtons, queso sardo rayado, tomates cherrys y aderezo caesar.",
+            "image": "/img/Caesar.jepg",
+            "price": 850,
+            "status": "MasVendido"
+        },
+        {
+            "id": 4,
+            "name": "Crispy",
+            "base": "Mix de verdes",
+            "dressing": "Caesar",
+            "description": "Mix de verdes, pollo rebozado con pan rallado y copos de maiz, croûtons, queso sardo rayado, tomates cherrys y aderezo caesar.",
+            "image": "/img/Crispy.jng",
+            "price": 850,
+            "status": "MasVendido"
+        },
+        {
+            "id": 5,
+            "name": "Pastrón",
+            "base": "Rúcula y fideos(penne rigatti)",
+            "dressing": "Reducción de aceto balsámico",
+            "description": "Rúcula, penne rigatti, pastrón feteado, queso tybo en cubos, morron rojo de conserva, nueces y reducción dulce de aceto balsámico.",
+            "image":  "/img/Pastron.jng",
+            "price": 870
+        },
+        {
+            "id": 6,
+            "name": "Peras",
+            "base": "Rúcula",
+            "dressing": "Vinagreta (Oliva, Aceto, Vinagre y Pimienta)",
+            "description": "Rúcula, pollo, queso azul, nueces, peras en almíbar y vinagreta.",
+            "image":  "/img/Peras.jng",
+            "price": 850
+        },
+        {
+            "id": 8,
+            "name": "Crudo",
+            "base": "Rúcula y fideos(penne rigatti)",
+            "dressing": "Reducción dulce de aceto balsámico y pesto genovés",
+            "description": "Rúcula, penne rigatti, jamón crudo feteado, queso mozzarela en trozos, tomates secos, reducción dulce de aceto balsámico y pesto genovés.",
+            "image": "/img/Crudo.jpg",
+            "price": 870
+        },
+        {
+            "id": 9,
+            "name": "Brie",
+            "base": "Mix de verdes",
+            "dressing": "Mostaza y miel",
+            "description": "Mix de verdes, pollo, queso brie, tomates cherrys, croûtons, nueces, y aderezo de mostaza y miel.",
+            "image": "/img/Brie.jepg",
+            "price": 870
+        },
+        {
+            "id": "10",
+            "name": "Atún",
+            "base": "Rúcula",
+            "dressing": "Vinagreta",
+            "description": "Mix de verdes, atún de lata (en lomitos), brócoli, arroz yamani, tomates cherrys, cebolla morada y vinagreta.",
+            "image": "/img/Atun.JPG",
+            "price": 750
+        },
+        {
+            "id": "11",
+            "name": "Frutilla",
+            "base": "Rúcula",
+            "dressing": "Reducción de Aceto Balsámico",
+            "description": "Rúcula, frutilla, panceta, queso brie, nueces y reducción dulce de aceto balsámico.",
+            "image": "/img/Frutillas.png",
+            "price": 870,
+            
+            
+        }
+    ]
+     export default products
