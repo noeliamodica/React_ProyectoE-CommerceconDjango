e50d1cad87d33ae2d7699fe1817ead5bcdf7b358
noeliamodica <113479035+noeliamodica@users.noreply.github.com>
2023-04-06 17:01:45 -0300

    trabajo en logueo

--- src/screens/LoginScreen.js
+++ src/screens/LoginScreen.js
@@ -2,12 +2,52 @@ import React, { useState, useEffect} from 'react'
 import { Link} from 'react-router-dom'
 import {Form, Row, Button, Col }  from 'react-bootstrap';
 import { useSelector, useDispatch } from 'react-redux'
+import FormContainer from '../components/FormContainer';
 
 export default function LoginScreen() {
     const [email, setEmail] = useState('')
-    const [password, setPasword] = useState('')
+    const [password, setPassword] = useState('')
+
+    const submitHandler = (e)=> {
+        e.preventDefault()
+        console.log('Submitted')
+    }
 
   return (
-    <div>LoginScreen</div>
+    <FormContainer>
+        <h1> Sign In </h1>
+        <Form onSubmit={submitHandler}>
+            <Form.Group controlId='email'>
+                <Form.Label> Email Address </Form.Label>
+                <Form.Control 
+                    type='email'
+                    placeholder='Enter email'
+                    value={email}
+                    onChange={(e)=> setEmail(e.target.value) }
+                >
+                </Form.Control>
+            </Form.Group>
+
+            <Form.Group controlId='password'>
+                <Form.Label> Password </Form.Label>
+                <Form.Control 
+                    type='password'
+                    placeholder='Enter password'
+                    value={password}
+                    onChange={(e)=> setPassword(e.target.value) }
+                >
+                </Form.Control>
+            </Form.Group>
+
+            <Button type='submit' variant='primary'>  Sign In</Button>
+
+        </Form>
+
+        <Row className='py-3'>
+            <Col>
+                
+            </Col>
+        </Row>
+    </FormContainer>
   )
 }
