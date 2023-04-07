216a6304be5de568eca5297e59f9c412516d178b
noeliamodica <113479035+noeliamodica@users.noreply.github.com>
2023-04-07 18:45:08 -0300

    addToCart

--- src/screens/CartScreen.js
+++ src/screens/CartScreen.js
@@ -1,6 +1,25 @@
-import React from 'react'
+import React, { useEffect} from 'react'
+import { Link, useNavigate , useLocation, useParams} from 'react-router-dom'
+import { useDispatch, useSelector } from 'react-redux'
+import { Row, Col, ListGroup, Image, Form, Button, Card} from 'react-bootstrap'
+import { Message } from '../components/Message' 
+import { addToCart } from '../actions/cartActions'
+
 
 function CartScreen() {
+
+  const dispatch = useDispatch()
+  const { id } = useParams()
+
+  const location = useLocation()
+  const qty = parseInt(new URLSearchParams(location.search).get('qty')) 
+  
+  useEffect(() =>{
+    if (id){
+      dispatch(addToCart(id, qty))
+    }
+  }, [dispatch, id, qty] )
+
   return (
     <div>CartScreen</div>
   )
